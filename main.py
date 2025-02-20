cd9e097800e9d09ff839ce0ec2cc4007e6c26c81
Kippie <jenstheminer@gmail.com>
2025-02-20 23:45:05 +0100

    yes

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ def main(page: ft.Page):
     page.title = "VisionAI Desktop"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
+    loadingBar = ft.ProgressBar(width=400, visible=False)
+
+    enableVision = ft.Switch(label="Use vision", value=True)
+
     answer = ft.TextField(
             value="Hi, i'm your personal AI assistant, what can i help you with today?",
+            label="Answer",
             multiline=True,
             min_lines=1,
             max_lines=8,
@@ -22,42 +27,64 @@ def main(page: ft.Page):
     prompt = ft.TextField(label="Prompt",)
 
     def prompt_send(e):
+        loadingBar.visible = True
+        page.update()
         messages=[]
-        cam_port = 0
-        cam = VideoCapture(cam_port)
-        result, image = cam.read()
-        if result:
-            imshow("img", image)
-            imwrite("img.png", image)
-            waitKey(0)
-            destroyWindow("img")
 
-        response2: ChatResponse = chat(model='moondream', messages=[
-            {
-                'role': 'user',
-                'content': "Describe the image",
-                'images': ["./img.png"]
-            },
-        ])
 
-        print("Vision model report: " + response2.message.content + "\n")
+        if enableVision.value is True:
+            cam_port = 0
+            cam = VideoCapture(cam_port)
+            result, image = cam.read()
+            if result:
+                imshow("img", image)
+                imwrite("img.png", image)
+                waitKey(0)
+                destroyWindow("img")
+            response2: ChatResponse = chat(model='moondream', messages=[
+                {
+                    'role': 'user',
+                    'content': "Describe the image",
+                    'images': ["./img.png"]
+                },
+            ])
 
-        print(prompt.value)
+            print("Vision model report: " + response2.message.content + "\n")
 
-        response: ChatResponse = chat(model='openhermes', messages=messages + [
-            {
-                'role': 'user',
-                'content': "Context: You are a personal assistant, you have access to a camera, don't mention what you can see in the camera if it isn't useful to the prompt, the feed is now seeing: " + response2.message.content + ". Prompt: " + prompt.value,
-            },
-        ])
+            print(prompt.value)
+
+            response: ChatResponse = chat(model='openhermes', messages=messages + [
+                {
+                    'role': 'user',
+                    'content': "Context: You are a personal assistant, you have access to a camera, don't mention what you can see in the camera if it isn't useful to the prompt, the feed is now seeing: " + response2.message.content + ". Prompt: " + prompt.value,
+                },
+            ])
+        else:
+            response: ChatResponse = chat(model='openhermes', messages=messages + [
+                {
+                    'role': 'user',
+                    'content': "Context: You are a personal assistant. Prompt: " + prompt.value,
+                },
+            ])
+
         answer.value = response.message.content
         messages += [
             {'role': 'user', 'content': prompt.value},
             {'role': 'assistant', 'content': response.message.content},
         ]
+
+        loadingBar.visible = False
         page.update()
 
     page.add(
+        ft.Row(
+            [
+
+                loadingBar
+
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
         ft.Row(
             [
 
@@ -78,6 +105,15 @@ def main(page: ft.Page):
             [
 
                 ft.FilledButton(text="Send", icon="SEND", on_click=prompt_send)
+
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            [
+
+                enableVision,
+                ft.Icon(name=ft.Icons.REMOVE_RED_EYE_OUTLINED)
 
             ],
             alignment=ft.MainAxisAlignment.CENTER,
